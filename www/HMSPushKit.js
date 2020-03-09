f9e8fdfb05e2d208068988ecb55c11334225dd70
ikamaru <ikamaru.ikuane@gmail.com>
2020-03-09 16:22:09 +0100

    add subscribe

--- www/HMSPushKit.js
+++ www/HMSPushKit.js
@@ -12,3 +12,9 @@ exports.isGMSAvailable = function (success,error) {
 exports.getToken = function (success, error) {
     exec(success, error,'HMSPushKit', 'getToken', []);
 };
+exports.subscribeToTopic = function (topic,success, error) {
+    exec(success, error,'HMSPushKit', 'subscribeToTopic', [topic]);
+};
+exports.unsubscribeFromTopic = function (topic,success, error) {
+    exec(success, error,'HMSPushKit', 'unsubscribeFromTopic', [topic]);
+};
